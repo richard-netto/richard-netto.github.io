3f32b728dbbd1bcc8bf6f9fde95864c686d5bb43
Richard-Netto <richard.v.s.netto@gmail.com>
2023-04-24 21:58:22 -0300

    fixed button layout

--- src/components/VSHeaderMenu/components/WindowLayoutButton/components/BaseButton/BaseButton.tsx
+++ src/components/VSHeaderMenu/components/WindowLayoutButton/components/BaseButton/BaseButton.tsx
@@ -11,7 +11,7 @@ const BaseButton: React.FunctionComponent<IBaseButton> = ({
   <button
     type="button"
     title="LayoutButton"
-    className="m-[0.125rem] flex items-center justify-center rounded-sm p-[0.1875rem] hover:bg-dark-header-button-hover"
+    className="m-[0.125rem] flex items-center justify-center rounded-md p-[0.1875rem] hover:bg-dark-header-button-hover"
   >
     {children}
   </button>
