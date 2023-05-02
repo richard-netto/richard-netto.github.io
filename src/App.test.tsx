9898cf74e574bf5d6be73db02649245b51cc058c
Richard-Netto <richard.v.s.netto@gmail.com>
2023-05-02 20:43:54 -0300

    fix: Deploy script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,3 @@
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i) as HTMLElement;
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// test('renders example-repo link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/example-repo/i) as HTMLElement;
-//   expect(linkElement).toBeInTheDocument();
-// });
+import React from 'react';
 
 test.todo('');
